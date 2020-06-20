319c402fc62b1cb465e7c415083022e1ca46eb17
JaydenThunberg <thunberg.jayden@gmail.com>
2020-06-20 10:41:07 -0500

    reusable template repo created

---
+++ src/components/_template/_template.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class _template extends Component {
+  // Renders the entire _template on the DOM
+  render() {
+    return (
+      <div className="_template">
+        <p>_template</p>
+      </div>
+    );
+  }
+}
+
+export default _template;
