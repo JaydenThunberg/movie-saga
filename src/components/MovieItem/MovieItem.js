00412dd7a8e14da711bbfeb36ec92c00110daf55
JaydenThunberg <thunberg.jayden@gmail.com>
2020-06-20 12:07:55 -0500

    individual movie title, movie, and poster render to DOM

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,15 +1,26 @@
 //This will be a grid list of all movies with a push request to '/details'
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 class MovieItem extends Component {
   // Renders the entire MovieItem on the DOM
   render() {
+    const poster = this.props.item.poster;
+    const title = this.props.item.title;
+    const description = this.props.item.description;
+    let singlePoster = <img src={poster} alt={title} />
     return (
       <div className="MovieItem">
-        <p>MovieItem</p>
+        {console.log(this.props.item)}
+        {title}
+        {singlePoster}
+        {description}
+
       </div>
     );//end return
   }//end render
 }// end class
 
-export default MovieItem;
+const putReduxStateOnProps = (reduxState) => ({ reduxState });
+
+export default connect(putReduxStateOnProps)(MovieItem);
