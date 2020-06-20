f02ec9a1bb4957fc4da9cead0c4ea1605c621d8e
JaydenThunberg <thunberg.jayden@gmail.com>
2020-06-20 11:03:21 -0500

    hashrouter and routes working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import MovieList from '../MovieList/MovieList';
+import MovieItem from '../MovieItem/MovieItem';
+import Edit from '../Edit/Edit';
+import Details from '../Details/Details';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <h1>Lordess of the Reels <span role="img" aria-label="video camera recorder">🎥</span></h1>
+        <Router>
+          <Route exact path="/" component={MovieList} />
+          <Route path="/details" component={Details} />
+          <Route path="/edit" component={Edit} />
+        </Router>
       </div>
     );
   }
